0d55e84150582808b063b0e4b966163ff27d72a2
Leyla <72270606+LeysSources@users.noreply.github.com>
2024-04-23 16:37:15 -0400

    Changeed numOf variables to ints instead of strings. Adds metaStratedgies so the computer has defences against longer and shorter patterns. As well as increasing the result font size to fill the extra space. ALong with centering the p[rogram in the center of the screen so i stop hurting my neck staring at the top right corner >~<

--- src/RockPaperScissorsGame.java
+++ src/RockPaperScissorsGame.java
@@ -11,7 +11,10 @@ import java.util.Random;
 public class RockPaperScissorsGame extends JFrame {
     private int playerScore = 0;
     private String playerMoveHistory = "";
+    private String[] allChoices = new String[4];
     private int computerScore = 0;
+    private int[] metaChoices = new int[4];
+
 
     private JLabel playerScoreLabel;
     private JLabel computerScoreLabel;
@@ -22,7 +25,6 @@ public class RockPaperScissorsGame extends JFrame {
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBackground(Color.decode("#ECECEC"));
-
         playerScoreLabel = new JLabel("Player: " + playerScore);
         playerScoreLabel.setFont(new Font("Arial", Font.PLAIN, 12));
         playerScoreLabel.setForeground(Color.BLUE);
@@ -32,7 +34,8 @@ public class RockPaperScissorsGame extends JFrame {
         computerScoreLabel.setForeground(Color.RED);
 
         resultLabel = new JLabel("");
-        resultLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        resultLabel.setFont(new Font("Arial", Font.PLAIN, 24)); // Fill some space
+        resultLabel.setVerticalAlignment(0);
         resultLabel.setHorizontalAlignment(JLabel.CENTER);
 
         String[] choices = {"Rock", "Paper", "Scissors"};
@@ -48,7 +51,8 @@ public class RockPaperScissorsGame extends JFrame {
         });
 
         setLayout(new FlowLayout());
-
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(screenSize.width/2 - this.getWidth() /2, screenSize.height/2 -  this.getHeight() /2);
         JMenuBar menuBar = new JMenuBar();
         JMenu gameMenu = new JMenu("Game");
         JMenuItem newGameMenuItem = new JMenuItem("New Game");
@@ -78,7 +82,6 @@ public class RockPaperScissorsGame extends JFrame {
         add(playerScoreLabel);
         add(computerScoreLabel);
         add(resultLabel);
-
         setVisible(true);
     }
     private String findPattern(int minLength)       //  this.playerMoveHistory is the data from our game
@@ -90,9 +93,9 @@ public class RockPaperScissorsGame extends JFrame {
         for(int index = this.playerMoveHistory.length() - minLength ; index  >= 0 ; index--)       // Reformat this and look it over
         {                                                                                          // just a quick bandaid for now (I am very sleepy) :P
 
-            String numOfR = "";         //  These are a local form of our global "r", "s", "p" variables.
-            String numOfS = "";         //  We use these to compare with the global count variables at the end
-            String numOfP = "";         //         of each pattern checking phase.
+            int numOfR = 0;         //  These are a local form of our global "r", "s", "p" variables.
+            int numOfS = 0;         //  We use these to compare with the global count variables at the end
+            int numOfP = 0;         //         of each pattern checking phase.
 
             String find = this.playerMoveHistory.substring(index);        //  find stores the pattern we are searching for.
 
@@ -101,31 +104,31 @@ public class RockPaperScissorsGame extends JFrame {
             if (!find.isEmpty())
             {
                 for (int pos = find.length(); pos < this.playerMoveHistory.length(); pos++)   //    We initialize pos as the end index while
-                {                                                                             //       iterating through the global "this.playerMoveHistory"
+                {                                                          //       iterating through the global "this.playerMoveHistory"
 
-                    if (this.playerMoveHistory.substring(historyIndex, pos).equals(find))     //   Comparing our gathered value to our pattern in "find".
+                    if (this.playerMoveHistory.substring(historyIndex, pos).equals(find))    //   Comparing our gathered value to our pattern in "find".
                     {
                         switch (this.playerMoveHistory.substring(pos, pos + 1))    //   Whatever letter comes after our pattern should be
                         {                                                          //       recorded into our local "numOf" variables.
-                            case "r" -> numOfR = numOfR.concat("r");
-                            case "s" -> numOfS = numOfS.concat("s");
-                            case "p" -> numOfP = numOfP.concat("p");
+                            case "r" -> numOfR++;
+                            case "s" -> numOfS++;
+                            case "p" -> numOfP++;
                         }
                     }
                     historyIndex++;    //  Moves the base index of "this.playerMoveHistory" along :P
                 }
             }
-            if (numOfR.length() > r)
+            if (numOfR > r)
             {
-                r = numOfR.length();        //
+                r = numOfR;                 //
             }                               //
-            if (numOfS.length() > s)        //  Simply moves the local "numOfR" variables into the
+            if (numOfS > s)                 //  Simply moves the local "numOfR" variables into the
             {                               //      global "r","s","p" variables if they are longer.
-                s = numOfS.length();        //
+                s = numOfS;                 //
             }                               //
-            if (numOfP.length() > p)
+            if (numOfP > p)
             {
-                p = numOfP.length();
+                p = numOfP;
             }
 
         }
@@ -154,24 +157,74 @@ public class RockPaperScissorsGame extends JFrame {
 
         }
 
-        //   We can improve on this by adding meta strategies
+        //   We improve on this by adding meta strategies
         //   which use this algorithm with different min lengths
         //   and play the one that is correct most often.
 
     }
 
+    private String pickBestPatternStartLength()     //  Decides which base length to use.
+    {                                               //  A larger base length is more effective at picking longer patterns.
+                                                    //  Very nice to have since player may not use the same short patterns again and again.
+        allChoices[0] = findPattern(0);
+        allChoices[1] = findPattern(3);
+        allChoices[2] = findPattern(5);
+        allChoices[3] = findPattern(8);
+        int highestScore =0 ;
+        int bestIndex = 0;
+        int index = 0;
+        for (int score : metaChoices)
+        {
 
+            if (score >= highestScore)
+            {
+                highestScore = score;
+                bestIndex = index;
+            }
+            index++;
+        }
+
+    return allChoices[bestIndex];
+
+    }
+
+    private void recordMetaRecords(String computerChoice, String result )   //  Just sees if the computer is winning or losing.
+    {                                                                       //  Then adjusts how accurate the pattern is projected to be.
+        if (result.equalsIgnoreCase("You win!"))
+        {
+            for(int index = 0; index < metaChoices.length; index++)
+            {
+                if(allChoices[index].equals(computerChoice))
+                {
+                    metaChoices[index]--;
+                }else
+                {
+                    metaChoices[index]++;
+                }
+            }
+        } else if (result.equalsIgnoreCase("Computer wins!"))
+        {
+            for (int index = 0; index < metaChoices.length; index++) {
+                if (allChoices[index].equals(computerChoice)) {
+                    metaChoices[index]++;
+                } else {
+                    metaChoices[index]--;
+                }
+            }
+
+        }
+    }
     private void removeOldHistory(int purgeLength)  //  "purgeLength"
     {                                               //      length of old data which will be removed.
         this.playerMoveHistory = this.playerMoveHistory.substring(purgeLength);
     }
     private void play(String playerChoice) {
-        String computerChoice = findPattern(3);
+        String computerChoice = pickBestPatternStartLength();
 
         String result = determineWinner(playerChoice, computerChoice);
         playerChoice = playerChoice.substring(0,1);
         playerChoice = playerChoice.toLowerCase();
-
+        recordMetaRecords(computerChoice, result);
         this.playerMoveHistory = this.playerMoveHistory.concat(playerChoice);
 
         if(this.playerMoveHistory.length() > 20)    //  Too much old data can result in inaccurate results
