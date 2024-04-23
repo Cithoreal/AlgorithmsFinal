bde12d1957b10b11e49b763158febae8474b6335
Leyla <72270606+LeysSources@users.noreply.github.com>
2024-04-23 15:14:05 -0400

    Added pattern finding algorithm and a method for removing game data history.

--- src/RockPaperScissorsGame.java
+++ src/RockPaperScissorsGame.java
@@ -10,6 +10,7 @@ import java.util.Random;
 
 public class RockPaperScissorsGame extends JFrame {
     private int playerScore = 0;
+    private String playerMoveHistory = "";
     private int computerScore = 0;
 
     private JLabel playerScoreLabel;
@@ -80,12 +81,104 @@ public class RockPaperScissorsGame extends JFrame {
 
         setVisible(true);
     }
+    private String findPattern(int minLength)       //  this.playerMoveHistory is the data from our game
+    {                                               //  minLength is the min size of our pattern
+        int r = 0;         //
+        int s = 0;         //   These are the highest amount of each move found during the pattern checking phase.
+        int p = 0;         //
 
+        for(int index = this.playerMoveHistory.length() - minLength ; index  >= 0 ; index--)       // Reformat this and look it over
+        {                                                                                          // just a quick bandaid for now (I am very sleepy) :P
+
+            String numOfR = "";         //  These are a local form of our global "r", "s", "p" variables.
+            String numOfS = "";         //  We use these to compare with the global count variables at the end
+            String numOfP = "";         //         of each pattern checking phase.
+
+            String find = this.playerMoveHistory.substring(index);        //  find stores the pattern we are searching for.
+
+            int historyIndex = 0;     // We use "historyIndex" as the starting index while we iterate through the global "this.playerMoveHistory"
+
+            if (!find.isEmpty())
+            {
+                for (int pos = find.length(); pos < this.playerMoveHistory.length(); pos++)   //    We initialize pos as the end index while
+                {                                                                             //       iterating through the global "this.playerMoveHistory"
+
+                    if (this.playerMoveHistory.substring(historyIndex, pos).equals(find))     //   Comparing our gathered value to our pattern in "find".
+                    {
+                        switch (this.playerMoveHistory.substring(pos, pos + 1))    //   Whatever letter comes after our pattern should be
+                        {                                                          //       recorded into our local "numOf" variables.
+                            case "r" -> numOfR = numOfR.concat("r");
+                            case "s" -> numOfS = numOfS.concat("s");
+                            case "p" -> numOfP = numOfP.concat("p");
+                        }
+                    }
+                    historyIndex++;    //  Moves the base index of "this.playerMoveHistory" along :P
+                }
+            }
+            if (numOfR.length() > r)
+            {
+                r = numOfR.length();        //
+            }                               //
+            if (numOfS.length() > s)        //  Simply moves the local "numOfR" variables into the
+            {                               //      global "r","s","p" variables if they are longer.
+                s = numOfS.length();        //
+            }                               //
+            if (numOfP.length() > p)
+            {
+                p = numOfP.length();
+            }
+
+        }
+
+        if (p > r && p > s)
+        {
+            return "Scissors";
+        }                                             //
+        else if (r > p && r > s)                      //
+        {                                             //   Tries to find which global count appears more
+            return "Paper";                           //
+        }                                             //
+        else if (s > p && s > r)
+        {
+            return "Rock";
+        }
+        else
+        {
+            Random rand = new Random();
+            return switch (rand.nextInt(3))
+            {                                              //   Otherwise we simply leave it up to random chance.
+                case 0 -> "Rock";
+                case 1 -> "Paper";
+                default -> "Scissors";
+            };
+
+        }
+
+        //   We can improve on this by adding meta strategies
+        //   which use this algorithm with different min lengths
+        //   and play the one that is correct most often.
+
+    }
+
+
+    private void removeOldHistory(int purgeLength)  //  "purgeLength"
+    {                                               //      length of old data which will be removed.
+        this.playerMoveHistory = this.playerMoveHistory.substring(purgeLength);
+    }
     private void play(String playerChoice) {
-        String[] choices = {"Rock", "Paper", "Scissors"};
-        String computerChoice = choices[new Random().nextInt(choices.length)];
+        String computerChoice = findPattern(3);
 
         String result = determineWinner(playerChoice, computerChoice);
+        playerChoice = playerChoice.substring(0,1);
+        playerChoice = playerChoice.toLowerCase();
+
+        this.playerMoveHistory = this.playerMoveHistory.concat(playerChoice);
+
+        if(this.playerMoveHistory.length() > 20)    //  Too much old data can result in inaccurate results
+        {
+            removeOldHistory(5);
+        }
+
         updateScores(result);
 
         resultLabel.setText("<html><div style='text-align: center;'>Computer chose: " + computerChoice + "<br>" + result + "</div></html>");
